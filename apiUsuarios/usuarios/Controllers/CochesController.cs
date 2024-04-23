548052118dd7392a328cf45016963a16a65d90f4
Anaicm <anaicm1@gmail.com>
2024-04-23 10:10:24 +0200

    controlador para la entidad coches

--- apiUsuarios/usuarios/Controllers/CochesController.cs
+++ apiUsuarios/usuarios/Controllers/CochesController.cs
@@ -29,12 +29,42 @@ public class CochesController : ControllerBase
     }
 
 
-    [HttpPost]
-    public IActionResult Add(string color, string modelo)
+    [HttpPost]//añadir
+    public IActionResult Add(string color, string modelo)//parametros que entra son los campos que se han añadido a la tabla en la clase entidades
     {
         _cochesContext.Coches.Add(new entidades.Coche { Color = color, modelo = modelo });
         _cochesContext.SaveChanges();
         return Ok(_cochesContext.Coches);
+    }
+    [HttpPut]//=> Modificar
+    //string? modelo => lleva la interrogacion para hacer opcional el campo que se va a modificar, es decir que no se tiene por que modificar ese
+    //campo solo el que se quiera modificar, lo mismo se hace con todos los campos, de esta forma solo puedo cambiar un campo y lo demas se quede 
+    //tal cual
+    public async Task<ActionResult<Coche>> Put(Guid id, string? color, string? modelo)
+    {
+        var cocheActualizar = _cochesContext.Coches.Find(id);//Find(id) => se usa cuando el campo es clavle primaria, casa que voy a actualizar por id
+        //var casa = _context.Casas.Where(c => c.id == id); => si el campo que se va a usar no es clave primaria
+
+        if (cocheActualizar != null)//si la casa tiene algun valor, es decir existe
+        {
+            //actualiza los campos de la entidades/Casas
+            //si no pongo esta condicion y cuando cambio un solo campo me lo actualiza con null, de esta forma deja el campo con el contenido 
+            //que tiene
+            if (color != null)
+            {
+                cocheActualizar.Color = color;//actualiza la casa con el color que me entra por parámetro
+            }
+            //si no pongo esta condicion y cuando cambio un solo campo me lo actualiza con null, de esta forma deja el campo con el contenido 
+            //que tiene
+            if (modelo != null)
+            {
+                cocheActualizar.modelo = modelo;//actualiza la casa con el modelo que me entra por parámetro
+            }
+        }
+        _cochesContext.SaveChanges();
+        return Ok();
+
+
     }
 
     [HttpDelete]//=> Borrar

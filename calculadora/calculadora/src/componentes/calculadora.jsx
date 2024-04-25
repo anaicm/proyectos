11f94dffc3a324c1d9a16173a545a34eb212f636
Anaicm <anaicm1@gmail.com>
2024-04-25 10:50:55 +0200

    uso de redux para manejar los estados en el componente

--- calculadora/calculadora/src/componentes/calculadora.jsx
+++ calculadora/calculadora/src/componentes/calculadora.jsx
@@ -4,8 +4,16 @@ import "./calculadora.css";
 import Boton from "./boton";
 import Switch from "./switch";
 
+//archivos para redux
+import { useAppDispatch, useAppSelector } from "../reducers/store.ts";
+import appSlice from "../reducers/appSlice.ts";
+
 const Calculadora = () => {
-  const [data, setData] = useState({ operacion: "", resultado: "" });
+
+  //hace falta si o si para redux
+  const appState = useAppSelector(); //
+  const dispatch = useAppDispatch(); //
+  
   //const escritura = (evento)=>{}//funcion
   const escritura = (evento) => {
     //evento.target.innerText => contiene el valor o el texto del boton, por lo que actualiza con
@@ -18,38 +26,38 @@ const Calculadora = () => {
       valor === "*" ||
       valor === "/" ||
       valor === "%";
-    if (data.operacion.length >= 10) return; //condicional para solo tener un maximo de 10 digitos
-    if (valor === "+/-" && data.operacion === "") return; //no pinta nada por pantalla
+    if (appState.data.operacion.length >= 10) return; //condicional para solo tener un maximo de 10 digitos
+    if (valor === "+/-" && appState.data.operacion === "") return; //no pinta nada por pantalla
     //si ya hay un porcentaje y contiene el signo porcentaje no pinte nada
-    if (valor === "%" && data.operacion.includes("%")) return;
+    if (valor === "%" && appState.data.operacion.includes("%")) return;
 
-    if (data.operacion.includes("Error") && valor !== "=" && valor !== "C") {
+    if (appState.data.operacion.includes("Error") && valor !== "=" && valor !== "C") {
       // si ya ha tenido un error anteriormente
       //cambia el valor de data operacion por el valor por el valor que recibe y limpia resultado
-      setData({ operacion: valor, resultado: "" });
+      dispatch(appSlice.actions.setData({ operacion: valor, resultado: "" }));
       return;
     } //si ya ha realizado alguna operación
-    else if (data.resultado !== "" && esOperacion) {
-      setData({
-        ...data,
-        operacion: data.resultado + valor, //coge el valor que hay en resultado y realiza la operacion siguiente
-      });
+    else if (appState.data.resultado !== "" && esOperacion) {
+      dispatch(appSlice.actions.setData({
+        ...appState.data,
+        operacion: appState.data.resultado + valor, //coge el valor que hay en resultado y realiza la operacion siguiente
+      }));
       return;
     }
-    if (valor === "+/-" && data.operacion !== "") {
+    if (valor === "+/-" && appState.data.operacion !== "") {
       //si se pulsa el boton +/- y hay un numero
-      if (data.operacion.slice(0, 1) === "-") {
+      if (appState.data.operacion.slice(0, 1) === "-") {
         //si el valor es negativo lo cambia a positivo
-        setData({
-          ...data,
-          operacion: data.operacion.slice(1, data.operacion.length),
-        });
+        dispatch(appSlice.actions.setData({
+          ...appState.data,
+          operacion: appState.data.operacion.slice(1, appState.data.operacion.length),
+        }));
       } else {
         //si el valor es positivo lo cambia a negativo
-        setData({
-          ...data,
-          operacion: "-" + data.operacion, //agrega el signo menos al inicio de la cadena
-        });
+        dispatch(appSlice.actions.setData({
+          ...appState.data,
+          operacion: "-" + appState.data.operacion, //agrega el signo menos al inicio de la cadena
+        }));
       }
     } else {
       //setData({...data})=>copia lo que tenga data exactamente hasta ese punto
@@ -57,22 +65,22 @@ const Calculadora = () => {
        * para guardar el valor en este caso de operación, se modifica la propiedad con el valor
        * que entra al hacer click es evento.target.innerText, sera el valor que tenga en el display
        */
-      setData({ ...data, operacion: data.operacion + valor });
+      dispatch(appSlice.actions.setData({ ...appState.data, operacion: appState.data.operacion + valor }));
     }
   };
   //función para ir borrando el último caracter que tenga el display
   const eliminar = () => {
     //operacion.slice(0,data.operacion.length -1) => va desde cero hasta la length -1
     //va a ir eliminando la ultima posición dejando los restantes.
-    setData({
-      ...data,
-      operacion: data.operacion.slice(0, data.operacion.length - 1),
-    });
+    dispatch(appSlice.actions.setData({
+      ...appState.data,
+      operacion: appState.data.operacion.slice(0, appState.data.operacion.length - 1),
+    }));
   };
   //función para borrar el estado completo del display
   const limpiar = () => {
     //se vuelve al estado iniciar
-    setData({ operacion: "", resultado: "" });
+    dispatch(appSlice.actions.setData({ operacion: "", resultado: "" }));
   };
   //función para mostrar el resultad de la operación
   const resultadoOperacion = () => {
@@ -94,34 +102,34 @@ const Calculadora = () => {
     try {
       //en try el codigo que quiero que se realice si todo está correcto
       let resultado; //eval(data.operacion);=> ya realiza la operación pero no realiza porcentajes
-      if (data.operacion.includes("%")) {
+      if (appState.data.operacion.includes("%")) {
         //si la operación es porcentaje
-        const valores = data.operacion.split("%"); //se separa por el porcentaje y se obtienen los dos valores
+        const valores = appState.data.operacion.split("%"); //se separa por el porcentaje y se obtienen los dos valores
         resultado = eval(`${valores[0]} * (${valores[1]} / 100)`); //se realiza la operación en la cadena
       } else {
         //si no
-        resultado = eval(data.operacion);
+        resultado = eval(appState.data.operacion);
       }
       //se actualiza setData resultado con la variable resultado
       //se actualiza setData operacion para que si hay un resultado se puedan seguir haciendo operaciones
-      setData({
-        ...data,
+      dispatch(appSlice.actions.setData({
+        ...appState.data,
         resultado,
         operacion: "",
-      });
+      }));
     } catch (error) {
       //el mensaje que dará si hay un error Ej: 8*9+= Error
-      setData({
-        ...data,
+      dispatch(appSlice.actions.setData({
+        ...appState.data,
         operacion: "Error",
-      });
+      }));
     }
   };
   return (
     <main>
       <Switch />
-      <span className="resultado">{data.resultado}</span>
-      <span className="display">{data.operacion}</span>
+      <span className="resultado">{appState.data.resultado}</span>
+      <span className="display">{appState.data.operacion}</span>
       {/**parametros que pasa al componente "Boton" */}
       <Boton texto="C" clase="gris" handleClick={limpiar} />
       {/**cambia de positivo a negativo o viceversa */}
